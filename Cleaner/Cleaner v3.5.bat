2cb5a8b6a9771ad359c2a6cdcb18bc80e73e6de6
BayEnesLOL3 <95964864+BayEnesLOL3@users.noreply.github.com>
2022-01-11 07:27:19 -0800

    Add files via upload

---
+++ Cleaner/Cleaner v3.5.bat
@@ -0,0 +1,791 @@
+@echo off
+title Downloading Files...
+
+color a
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading...        Please Wait
+echo ---------------------------------------
+echo                                 =   0% ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading.          Please Wait
+echo ---------------------------------------
+echo []                              =   5% ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading..         Please Wait
+echo ---------------------------------------
+echo [][]                            =  15% ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading...        Please Wait
+echo ---------------------------------------
+echo [][][]                          =  23%]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading.          Please Wait
+echo ---------------------------------------
+echo [][][][]                        =  30% ]
+echo ---------------------------------------
+ping localhost -n 4 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading..         Please Wait
+echo ---------------------------------------
+echo [][][][][]                      =  38% ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading...        Please Wait
+echo ---------------------------------------
+echo [][][][][]                      =  42% ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading.          Please Wait
+echo ---------------------------------------
+echo [][][][][][]                    =  45% ]
+echo ---------------------------------------
+ping localhost -n 1 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading..         Please Wait
+echo ---------------------------------------
+echo [][][][][][][]                  =  48% ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading...        Please Wait
+echo ---------------------------------------
+echo [][][][][][][][]                =  50% ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading.           Please Wait
+echo ---------------------------------------
+echo [][][][][][][][][]              =  56% ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading..         Please Wait
+echo ---------------------------------------
+echo [][][][][][][][][][]            =  63% ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading...        Please Wait
+echo ---------------------------------------
+echo [][][][][][][][][][][]          =  69% ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading.          Please Wait
+echo ---------------------------------------
+echo [][][][][][][][][][][][]        =  75% ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading..         Please Wait
+echo ---------------------------------------
+echo [][][][][][][][][][][][][]      =  79% ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading...        Please Wait
+echo ---------------------------------------
+echo [][][][][][][][][][][][][][]    =  86% ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading.          Please Wait
+echo ---------------------------------------
+echo [][][][][][][][][][][][][][][]  =  90% ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading..         Please Wait
+echo ---------------------------------------
+echo [][][][][][][][][][][][][][][]  =  96% ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading...        .Complete.
+echo ---------------------------------------
+echo [][][][][][][][][][][][][][][][]= 100% ]
+echo ---------------------------------------
+
+:: *************v REMOVE v*************
+echo.
+echo By @yotther (Andrei Nicolaidis)
+echo Downloaded Complete...
+echo Press any key to continue.....
+echo :]
+:: *************^ REMOVE ^*************
+
+pause >nul
+
+title Cleaner v3.5
+echo Downloaded Complete.....
+echo Cleaner Proximy 
+echo        *   *   *   *
+echo    *    *   *   *
+echo    *   *    *   *  *
+echo __________________________
+
+echo By Company Cleaner Proximy
+:start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+
+:start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+
+:start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+
+:start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+ 
+ :start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+ 
+ :start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+ 
+ :start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+ 
+ :start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+ 
+ :start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+ 
+ :start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+ 
+ :start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+ 
+ :start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+ 
+ :start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+ 
+ :start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+ 
+ :start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+ 
+ :start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+ 
+ :start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+ 
+ :start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+ 
+ :start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+ 
+ :start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+ 
+ :start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+ 
+ :start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+ 
+ :start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+ 
+ :start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+ 
+ :start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+ 
+ :start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+ 
+ :start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+ 
+ :start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+ 
+ :start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+ 
+
+ :start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+ 
+ :start
+echo Enter your file name here!
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected 
+if NOT EXIST %name% goto clean
+cd D:
+:infected 
+echo Good!
+DEL %name%
+pause
+goto start 
+:clean 
+echo Your file has been deleted! 
+pause
+ 
+echo Files has been deteled!
+echo Restart the program and delete again files!
+ 
+
+
